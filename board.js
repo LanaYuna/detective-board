d800c5347591a76de544957c7e783c634629da60
Lana Yuna Oyama <lanayu2007@gmail.com>
2026-01-21 23:55:18 -0300

    Implementação de figuras e lógica

--- board.js
+++ board.js
@@ -1,4 +1,10 @@
 const canvas = document.querySelector("canvas");
+const buttonCircle = document.querySelector("#button-circle");
+const buttonRectangle = document.querySelector("#button-rectangle");
+const buttonPencil = document.querySelector("#button-pencil");
+
+let objects = []; // armazenar em array de objetos para gerenciar estado
+let currentTool = 'pincel';
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -6,30 +12,121 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext("2d");
 
 let drawing = false;
+let placeCircle = false;
+let placeRectangle = false;
 
 let previousPosition = {
     x: 0,
     y: 0
 };
 
-window.addEventListener("mousedown", (event) => {
-    drawing = true;
-    previousPosition.x = event.offsetX;
-    previousPosition.y = event.offsetY;
+buttonPencil.addEventListener("click", () => {currentTool = 'pencil'})
+buttonCircle.addEventListener("click", () => {currentTool = 'circle'});
+buttonRectangle.addEventListener("click", () => {currentTool = 'rectangle'})
+
+canvas.addEventListener("mousedown", (event) => {
+    const newObject = {
+        x : event.offsetX,
+        y: event.offsetY,
+        type: currentTool,
+        color: 'black'
+    };
+
+    objects.push(newObject);
+
+    renderDrawing();
 })
 
-window.addEventListener("mousemove", (event) => {
-    if(!drawing) return;
+function renderDrawing(){
 
-    c.beginPath();
-    c.moveTo(previousPosition.x, previousPosition.y);
-    c.lineTo(event.offsetX, event.offsetY);
-    c.lineCap = 'round';
-    c.stroke();
-    previousPosition.x = event.offsetX;
-    previousPosition.y = event.offsetY;
-})
+    c.clearRect(0, 0, canvas.width, canvas.height); // limpa o quadro
 
-window.addEventListener("mouseup", () => {
-    drawing = false;
-})
+    objects.forEach(obj => { // redesenha a todo momento
+        c.beginPath();
+
+        if(obj.type == 'circle'){
+            c.arc(obj.x, obj.y, 20, 0, Math.PI * 2, false);
+            c.stroke();
+        } else if(obj.type == 'rectangle'){
+            c.strokeRect(obj.x, obj.y, 20, 20);
+        } else if(obj.type == 'pencil'){
+            drawing = true;
+            previousPosition.x = obj.x;
+            previousPosition.y = obj.y;
+
+            canvas.addEventListener("mousemove", (event) => {
+                if(!drawing) return;
+
+                c.beginPath();
+                c.moveTo(previousPosition.x, previousPosition.y);
+                c.lineTo(event.offsetX, event.offsetY);
+                c.lineCap = 'round';
+                c.stroke();
+                previousPosition.x = event.offsetX;
+                previousPosition.y = event.offsetY;
+            })
+
+            canvas.addEventListener("mouseup", () => {
+                drawing = false;
+            })
+        }
+    })
+
+    console.log(objects)
+};
+
+// buttonPencil.addEventListener("click", () => {
+//     placeCircle = false;
+//     placeRectangle = false;
+
+//     window.addEventListener("mousedown", (event) => {
+//         drawing = true;
+//         previousPosition.x = event.offsetX;
+//         previousPosition.y = event.offsetY;
+//     })
+
+//     window.addEventListener("mousemove", (event) => {
+//         if(!drawing) return;
+
+//         c.beginPath();
+//         c.moveTo(previousPosition.x, previousPosition.y);
+//         c.lineTo(event.offsetX, event.offsetY);
+//         c.lineCap = 'round';
+//         c.stroke();
+//         previousPosition.x = event.offsetX;
+//         previousPosition.y = event.offsetY;
+//     })
+
+//     window.addEventListener("mouseup", () => {
+//         drawing = false;
+//     })
+// })
+
+
+// buttonCircle.addEventListener("click", () => {
+//     placeCircle = true;
+//     placeRectangle = false;
+
+//     window.addEventListener("mousedown", (event) => {   
+//         if(!placeCircle) return;
+        
+//         drawing = false;   
+//         c.beginPath();
+//         c.arc(event.offsetX, event.offsetY, 10, 0, Math.PI * 2, false);
+//         c.stroke();
+
+//     })
+
+// })
+
+// buttonRectangle.addEventListener("click", () => {
+//     placeRectangle = true;
+//     placeCircle = false;
+
+//     window.addEventListener("mousedown", (event) => {
+//         if(!placeRectangle) return;
+
+//         drawing = false;
+//         c.strokeRect(event.offsetX, event.offsetY, 20, 20); 
+//     });
+
